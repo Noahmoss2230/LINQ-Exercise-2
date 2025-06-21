a6b53cb1fc2106f9c2d8b2623b922af3f3abfa5b
Noah Moss <noahmoss50@gmail.com>
2025-06-20 20:15:15 -0500

    finished Exercise 2

---
+++ LINQ-Exercise-2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQGameList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create and populate the list
+            List<string> games = new List<string>()
+            {
+                "Halo",
+                "The Legend of Zelda",
+                "Minecraft",
+                "Call of Duty",
+                "Among Us",
+                "Super Mario Bros",
+                "Portal",
+                "Elden Ring"
+            };
+
+            // Order the games by the length of their names using LINQ method syntax
+            var orderedGames = games.OrderBy(game => game.Length).ToList();
+
+            // Display the sorted list
+            Console.WriteLine("Games ordered by name length:");
+            foreach (var game in orderedGames)
+            {
+                Console.WriteLine($"{game} ({game.Length} characters)");
+            }
+        }
+    }
+}
